04bcfdff5c44ee76af8f843f834cb733058806db
Facundo Alemandi <facuinstituto10@gmail.com>
2022-09-29 01:11:34 -0300

    Acomodandop el nav btm

--- src/components/NavButtom/NavButtom.js
+++ src/components/NavButtom/NavButtom.js
@@ -1,47 +1,56 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-import Dark from '../../images/dark.png'
-import User from '../../images/user.png'
-import Cart from '../../images/cart.png'
+import Dark from "../../images/dark.png";
+import User from "../../images/user.png";
+import Cart from "../../images/cart.png";
 
 const Nav = styled.nav`
-height: auto;
-position: fixed;
-bottom: 0px;
-width: 100%;
-display: flex;
-justify-content: space-around;
-align-items: center;
-padding: 10px;
-border-top-left-radius: 30px;
-border-top-right-radius: 30px;
-box-shadow: 0px 0px 5px 5px rgba(0, 0, 0, 0.100);
-background-color: rgba(223, 223, 223, 0.435);
+  height: auto;
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  box-shadow: 0px 0px 5px 5px rgba(0, 0, 0, 0.1);
+  background-color: rgba(223, 223, 223, 0.435);
 
-div{
+  div {
     width: 70px;
     height: 70px;
     border-radius: 100%;
-}
-`
+  }
+`;
 const Img = styled.img`
-    width: 70px;
-    height: 70px;
-    border-radius: 5px;
-    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.200);
-    background-color: white;
-    padding: 15px;
-    border-radius: 20px;
-`
+  width: 70px;
+  height: 70px;
+  border-radius: 5px;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.2);
+  background-color: white;
+  padding: 15px;
+  border-radius: 20px;
+`;
+const SectionMenu = styled.section`
+  border: 1px solid red;
+  top: 0px;
+  width: 100%;
+  transform: translateX(100%);
+  transition: 2s;
+`;
 const NavButtom = () => {
   return (
-     <Nav>
-          <Img src={Dark}/>
-          <Img src={User}/>
-          <Img src={Cart}/>
-     </Nav>
-  )
-}
+    <main>
+      <Nav>
+        <Img src={Dark} />
+        <Img src={User} />
+        <Img src={Cart} />
+      </Nav>
+    </main>
+  );
+};
 
-export default NavButtom
+export default NavButtom;
